e8b8f4750100db50bc21bec68a169375d062dfdc
imnikhilmathew-max <imnikhilmathew@gmail.com>
2026-02-02 19:04:15 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,12 @@
   import { defineConfig } from 'vite';
   import react from '@vitejs/plugin-react-swc';
   import path from 'path';
+  import { defineConfig } from "vite";
+  import react from "@vitejs/plugin-react-swc";
+
+export default defineConfig({
+  plugins: [react()],
+  base: "/themora/" })
 
   export default defineConfig({
     plugins: [react()],
