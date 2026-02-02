eb4b8884256fb52ad2994eadef2b003e01cf03cf
imnikhilmathew-max <imnikhilmathew@gmail.com>
2026-02-02 20:21:19 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,12 @@
   import react from '@vitejs/plugin-react-swc';
   import path from 'path';
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "dist"
-       }
-})
 
   export default defineConfig({
     plugins: [react()],
+    build: {
+    outDir: "dist"
+       }
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
